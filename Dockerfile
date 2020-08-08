f6092b2b9d51f5ca79d5c626fb08caeb40f1bb90
Javi Cancela <cancelajavi@gmail.com>
2020-08-09 00:37:54 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:13.12.0-alpine as build
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm i
+COPY . ./
+RUN npm run build
+
+
+FROM nginx:stable-alpine
+COPY --from=build /app/public /usr/share/nginx/html
+EXPOSE 8001
+CMD ["nginx", "-g", "daemon off;"]
---
+++ gatsby-config.js
@@ -0,0 +1,65 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `xchappi`,
+    siteTitleAlt: `xChappi`,
+    siteHeadline: `xChappi - Software developer`,
+    siteUrl: `http://xchappi.com`,
+    siteDescription: `xChappi portfolio`,
+    siteLanguage: `es`,
+    siteImage: `/banner.jpg`,
+    author: `@xchappi`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        short_name: `Cara`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+};
